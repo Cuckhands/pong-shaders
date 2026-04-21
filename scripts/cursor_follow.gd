b5c64dba68b4651dd72cbdff41ec7c522f74423c
cuckhands <thepix3lator@gmail.com>
2026-04-21 03:01:32 -0400

    Attempt unmoving_plaid shader

--- scripts/cursor_follow.gd
+++ scripts/cursor_follow.gd
@@ -1,9 +1,21 @@
-extends Node2D
+extends Sprite2D
+
+func set_shader_position(new_pos: Vector2):
+	if material is ShaderMaterial:
+		material.set_shader_parameter("position", new_pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	# if I have a shader,
+	if material is ShaderMaterial:
+		# make sure changes to it don't affect others using this same material
+		material = material.duplicate()
+		
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	position = get_global_mouse_position()
+	
+	if material is ShaderMaterial:
+		set_shader_position(global_position)
