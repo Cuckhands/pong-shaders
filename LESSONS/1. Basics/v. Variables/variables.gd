3116a3542fcc3e15c24dd4151b22665f7fd02b50
cuckhands <thepix3lator@gmail.com>
2026-04-24 02:23:03 -0400

    Solve Lesson 1.5 Variables check

---
+++ LESSONS/1. Basics/v. Variables/variables.gd
@@ -0,0 +1,16 @@
+extends Node2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#$Sprite2D.material.set_shader_parameter('myColor', Color.ANTIQUE_WHITE)
+	var tween := create_tween()
+	tween.tween_property($Sprite2D.material, "shader_parameter/Offset", Vector2(64.0, 0), 1)
+	tween.tween_property($Sprite2D.material, "shader_parameter/Offset", Vector2(0, 0), 1)
+	tween.tween_property($Sprite2D.material, "shader_parameter/CircleRadius", 0.1, 1)
+	tween.tween_property($Sprite2D.material, "shader_parameter/CircleRadius", 0.5, 1)
+	tween.set_loops(INF)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
