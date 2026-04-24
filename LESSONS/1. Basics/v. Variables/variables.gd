a09a4a406a6fd78715294f8ec42b404595ebd5a2
cuckhands <thepix3lator@gmail.com>
2026-04-24 02:29:33 -0400

    Code cleanup

--- LESSONS/1. Basics/v. Variables/variables.gd
+++ LESSONS/1. Basics/v. Variables/variables.gd
@@ -3,13 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#$Sprite2D.material.set_shader_parameter('myColor', Color.ANTIQUE_WHITE)
 	var tween := create_tween()
 	tween.tween_property($Sprite2D.material, "shader_parameter/Offset", Vector2(64.0, 0), 1)
-	tween.tween_property($Sprite2D.material, "shader_parameter/Offset", Vector2(0, 0), 1)
+	tween.tween_property($Sprite2D.material, "shader_parameter/Offset", Vector2.ZERO, 1)
 	tween.tween_property($Sprite2D.material, "shader_parameter/CircleRadius", 0.1, 1)
 	tween.tween_property($Sprite2D.material, "shader_parameter/CircleRadius", 0.5, 1)
-	tween.set_loops(INF)
+	tween.set_loops()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
