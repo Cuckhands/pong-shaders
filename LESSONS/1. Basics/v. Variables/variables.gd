c4147bea635e944815bc54a9b4fcf26236428d1a
cuckhands <thepix3lator@gmail.com>
2026-04-25 00:34:14 -0400

    Finish Lesson 1.5 Variables

--- LESSONS/1. Basics/v. Variables/variables.gd
+++ LESSONS/1. Basics/v. Variables/variables.gd
@@ -3,12 +3,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var tween := create_tween()
-	tween.tween_property($Sprite2D.material, "shader_parameter/Offset", Vector2(64.0, 0), 1)
-	tween.tween_property($Sprite2D.material, "shader_parameter/Offset", Vector2.ZERO, 1)
-	tween.tween_property($Sprite2D.material, "shader_parameter/CircleRadius", 0.1, 1)
-	tween.tween_property($Sprite2D.material, "shader_parameter/CircleRadius", 0.5, 1)
-	tween.set_loops()
+	#var tween := create_tween()
+	#tween.tween_property($Sprite2D.material, "shader_parameter/Offset", Vector2(64.0, 0), 1)
+	#tween.tween_property($Sprite2D.material, "shader_parameter/Offset", Vector2.ZERO, 1)
+	#tween.tween_property($Sprite2D.material, "shader_parameter/CircleRadius", 0.1, 1)
+	#tween.tween_property($Sprite2D.material, "shader_parameter/CircleRadius", 0.5, 1)
+	#tween.set_loops()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
