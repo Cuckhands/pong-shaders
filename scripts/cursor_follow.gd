28eacc382b4b3067e33ca0402d55b2ce53b17edf
cuckhands <thepix3lator@gmail.com>
2026-05-03 15:56:43 -0400

    Add documentation to unmoving plaid

--- scripts/cursor_follow.gd
+++ scripts/cursor_follow.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Assign the shader texture its size if not already defined
 	if material is ShaderMaterial:
 		material = material.duplicate()
 		var bg_tex = material.get_shader_parameter("background_texture")
 		if bg_tex:
-			material.set_shader_parameter("texture_size", bg_tex.get_size())
+			var tx_size = material.get_shader_parameter("texture_size")
+			if tx_size == Vector2(0,0):
+				material.set_shader_parameter("texture_size", bg_tex.get_size())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
